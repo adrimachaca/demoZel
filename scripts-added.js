11a5a58e39155cb437e055dcad972f9076aa303a
Adri Machaca <adrimachacalopez@gmail.com>
2023-08-30 11:28:26 +0200

    commit 1

---
+++ scripts-added.js
@@ -0,0 +1,21 @@
+function demo1() {
+  picea.run(
+    { user: "teststore1@save", password: "TTTtest_save_123" },
+    onTransactionCompleted
+  );
+}
+
+function onTransactionCompleted(tr) {
+  var tituloText = document.getElementById("titulo");
+  tituloText.innerHTML = "Gracias por utilizar nuestro servicio de Trade-In";
+
+  var textoInfo = document.getElementById("texto-info");
+  textoInfo.innerHTML =
+    "Guarde el siguiente número de transacción para el procesa de recogida del dispositivo:";
+
+  var elementTransaction = document.getElementById("transaction");
+  elementTransaction.innerText = tr.number;
+
+  var startBtn = document.getElementById("boton-start-diagnostics");
+  startBtn.style.display = "none";
+}
